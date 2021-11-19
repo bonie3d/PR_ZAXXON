aa3fd781228c74f6b9ec040694c724e67653ee81
David <bonie3d@gmail.com>
2021-11-19 19:56:59 +0100

    Segunda entrega de Zaxxon David
    La práctica está incompleta, me faltan muchas cosas.

---
+++ NavePruebaBala/Assets/Scripts/MenuIncio.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuIncio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void EscenaJuego()
+    {
+        SceneManager.LoadScene("pruebanave");
+    }
+
+    public void CargarMenu(string menuinicio)
+       
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Salir ()
+    {
+        Application.Quit();
+    }
+}
